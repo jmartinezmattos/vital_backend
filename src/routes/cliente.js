00bd6d05bf93856ed6975643164ab1a2220f44d4
Joaquin <joaco1509@gmail.com>
2020-06-07 21:45:43 -0300

    commit

---
+++ src/routes/cliente.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+//Getting all
+router.get('/', (req, res)=> {
+    
+})
+
+//Getting one
+router.get('/:id', (req, res)=> {
+    
+})
+
+//Creating one
+router.post('/', (req, res)=> {
+    
+})
+
+//Updating one
+router.patch('/', (req, res)=> {
+    
+})
+
+//Deleting all
+router.delete('/:id', (req, res)=> {
+    
+})
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
-require('dotenv').config()
+//require('.env').config()
 
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
 
-mongoose.conect(process.env.DATABASE_URL, { useNewUrlParser: True})
 
-app.listen(3000, () => console.log('Server started at 3000'))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
 
-const db = mongooose.conection
+const db = mongoose.conection
 
 db.on('error', (error) => console.error(error))
 db.once('open', )
 
+//importing routes
+const indexRoutes = require('./src/routes/cliente');//le aviso que aca estan las rutas
+//routes
+app.use('/', indexRoutes);
 
+app.listen(3000, () => console.log('Server started at 3000'))
 
 
 
