57591c7c01a6a2579dfd510f79ad3256c9211505
Joaquin Martinez <joaco1509@gmail.com>
2020-06-13 20:07:20 -0300

    commit

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,4 +1,4 @@
-//https://www.youtube.com/watch?v=xMEOT9J0IvI&list=PLYQSCk-qyTW2ewJ05f_GKHtTIzjynDgjK&index=5
+
 
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
