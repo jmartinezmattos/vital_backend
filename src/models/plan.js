e94ec09fa8988fab0c0bb907bf9da487011ad1d5
Federico Abdo <fabdo@correo.um.edu.uy>
2020-07-12 20:25:54 -0300

    cambio el typo de dias en el squema de plan a ARRAY

--- src/models/plan.js
+++ src/models/plan.js
@@ -3,10 +3,8 @@ const Schema = mongoose.Schema;
 //const Dia = require('../models/day');
 
 const PlanSchema = new Schema({
-
     nombre: String,
-    dias: []
-
+    dias: Array
 })
 
 module.exports = mongoose.model('planes',PlanSchema)
